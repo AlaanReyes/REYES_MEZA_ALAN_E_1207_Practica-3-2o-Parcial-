23790c244c0f996a861707f41b9b7cca4bd552e0
AlaanReyes <a.23308051281207@cbtis128.edu.mx>
2024-10-15 01:13:35 -0600

    Delete REYES_MEZA_ALAN_EDUARDO_1207_programa3_Practica3 2o Parcial.py

--- REYES_MEZA_ALAN_EDUARDO_1207_programa3_Practica3 2o Parcial.py
+++
@@ -1,7 +0,0 @@
-frutas = {'Platano': 30, 'Manzana': 37, 'Pera': 10, 'Naranja': 25, 'Fresa': 50,'Mango': 32, 'Sandia': 34, 'Uva': 16}
-fruta = input('¿Qué fruta quieres? ').title()
-kg = float(input('¿Cuántos kilos? '))
-if fruta in frutas:
-    print(kg, 'kilos de', fruta, 'valen', frutas[fruta]*kg, 'pesos mexicanos')
-else: 
-    print("Una disculpa, la fruta", fruta, "no esta disponible.")
